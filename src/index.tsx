d547ff55f9d94b94201c3703fa235b42e8a6d4d3
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-18 16:34:09 -0600

    Add Router to index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './Components/App/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
