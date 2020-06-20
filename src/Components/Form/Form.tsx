e678ff349985e29448cf94b8fde29729562030df
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 17:36:10 -0600

    Add full width to name input and add content input

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
+import useStyles from './styles';
 
 const Form: React.FC = () => {
 
+  const classes = useStyles(); 
+
     return (
-        <form noValidate>
-            <TextField label='name' name='name ' />
+        <form noValidate className={classes.formContainer}>
+            <TextField label='Name' name='name ' fullWidth />
+            <TextField label='Content' name='content ' fullWidth multiline rows={3} />
         </form>
     )
 };
