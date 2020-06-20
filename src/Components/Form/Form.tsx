d40392c3329e093b3fa88d3f6c70d4183570c254
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 15:52:14 -0600

    Add basic layout for the form component

---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form: React.FC = () => {
+
+    return (
+      <section>
+          <h2>Form</h2>
+          <form>
+
+          </form>
+      </section>
+    )
+};
+
+export default Form;
