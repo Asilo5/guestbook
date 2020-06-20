37bf0dd881a85c26512441704b18ea457028068b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-19 21:11:04 -0600

    Add basic layout of home component and import it into app component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,9 +3,9 @@ import { Switch, Route, Redirect, Link } from 'react-router-dom';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Home from '../Home/Home';
 
 import { useStoreState } from '../../hooks';
 import useStyles from './styles';
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    
+    return (
+        <section>
+            <h1>Hello!</h1>
+        </section>
+    )
+};
+
+export default Home;
