93cc2e02aa51c436afb3fdc616b4d9ed3ad31a91
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-19 21:40:20 -0600

    Append guestbook entries to dom

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -5,17 +5,14 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import Home from '../Home/Home';
 
-import { useStoreState } from '../../hooks';
 import useStyles from './styles';
+import Home from '../Home/Home';
+import About from '../About/About';
 
 const App: React.FC = () => {
   
   const classes = useStyles();
-  const { entries } = useStoreState((state) => state.guestbook);
-
-  console.log(entries)
 
   return (
     <section className={classes.root}>
@@ -24,15 +21,15 @@ const App: React.FC = () => {
           <IconButton edge="start" color="inherit" aria-label="menu">
             🍔
           </IconButton>
-          <Button color="inherit"><Link to='/'>Home</Link></Button>
-          <Button color="inherit"><Link to='/about'>About</Link></Button>
+          <Button component={Link} to='/' color="inherit">Home</Button>
+          <Button component={Link} to='/about' color="inherit">About</Button>
         </Toolbar>
       </AppBar>
 
       <section>
          <Switch>
            <Route exact path='/' component={Home} /> 
-           {/* <Route exact path='/about' component={About}/> */}
+           <Route exact path='/about' component={About}/>
            <Redirect to='/' />
          </Switch>
        </section>
