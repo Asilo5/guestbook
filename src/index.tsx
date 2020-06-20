011842e9ab06726d5e43316995039e35bbffc765
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-19 20:02:53 -0600

    Add StoreProvider to be able to access the store from the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
 
 import './index.css';
 import App from './Components/App/App';
+import store from './store/index';
 
 ReactDOM.render(
     <React.StrictMode>
       <Router>
-        <App />
+        <StoreProvider store={store}>
+            <App />
+        </StoreProvider>
       </Router>
     </React.StrictMode>,
   document.getElementById('root')
