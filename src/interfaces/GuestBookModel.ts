d855bae32fc11522e6ee4ac7b612464fe0e6fd22
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 22:10:30 -0600

    Add fetch of local host with thunk

--- src/interfaces/GuestBookModel.ts
+++ src/interfaces/GuestBookModel.ts
@@ -1,9 +1,10 @@
 import GuestBookEntry from './GuestBookEntry'; 
-import { Action } from 'easy-peasy';
+import { Action, Thunk } from 'easy-peasy';
 
 // An array of Guest Book Entry
 export default interface GuestBookModel {
     entries: GuestBookEntry[];
-    addEntry:  Action<GuestBookModel, GuestBookEntry>; // 👈 declaring our action 
-                                                       // has two parameters, one the model and other the entry
+    setEntries: Action<GuestBookModel, GuestBookEntry[]>;
+    addEntry:  Action<GuestBookModel, GuestBookEntry>; // 👈 declaring our action, has two parameters, one the model and other the entry
+    getEntries: Thunk<GuestBookModel>
 };
--- src/store/GuestBook.ts
+++ src/store/GuestBook.ts
@@ -1,14 +1,22 @@
 // Implement the guest book model
 import GuestBookModel from '../interfaces/GuestBookModel';
-import { action } from 'easy-peasy';
+import { action, thunk } from 'easy-peasy';
 
 const GuestBook: GuestBookModel = {
   entries: [], // set initial state of entries --> there are no guest input
+  setEntries: action((state, entries) => {
+     state.entries = entries;
+  }),
   addEntry: action((state, entry ) => {
     entry.id = Date.now();
     entry.submitted = new Date();
     state.entries.unshift(entry);
-  })
+  }),
+  getEntries: thunk(async (state) => {
+     const response = await fetch('http://localhost:3001/entries');
+     const entries = await response.json; 
+     state.setEntries(entries);
+  }),
 };
 
 export default GuestBook;
