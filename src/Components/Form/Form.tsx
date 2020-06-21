c22c76f5ba334dc141ac3e0d0d857820c497cc6b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 18:37:42 -0600

    Add react hooks form package and add it to Form component

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box'; 
+
 import useStyles from './styles';
+import { useForm } from "react-hook-form";
 
 const Form: React.FC = () => {
 
   const classes = useStyles(); 
+  const { register, handleSubmit } = useForm();
+
+  const submitForm = (data): void => {
+     console.log(data);
+  };
 
     return (
-        <form noValidate className={classes.formContainer}>
-            <TextField label='Name' name='name ' fullWidth />
-            <TextField label='Content' name='content ' fullWidth multiline rows={3} />
+        <form onSubmit={handleSubmit(submitForm)} noValidate className={classes.formContainer}>
+            <TextField 
+               inputRef={register} 
+               label='Name' 
+               name='name ' 
+               fullWidth 
+            />
+            <TextField 
+               label='Content' 
+               name='content ' 
+               fullWidth 
+               multiline 
+               rows={3} 
+               variant='outlined' 
+               inputRef={register}
+            />
+            <Box display='flex' justifyContent='center'>
+                <Button variant="contained" color='primary'> Submit </Button>
+            </Box>
         </form>
     )
 };
