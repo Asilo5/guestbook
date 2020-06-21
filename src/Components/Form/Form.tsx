794c1022e0d4718f7a6f449a7e05f3e344726fe1
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 19:45:58 -0600

    Add errors to show on the dom when validation requirements not met

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -9,8 +9,16 @@ import { useForm } from "react-hook-form";
 import GuestBookEntry from '../../interfaces/GuestBookEntry';
 
 const GuestBookEntrySchema = yup.object().shape({
-   name: yup.string().trim().required(),
-   content: yup.string().trim().min(10).max(200).required()
+   name: yup
+        .string()
+        .trim()
+        .required('Required.'),
+   content: yup
+            .string()
+            .trim()
+            .min(10, 'Must be at least 10 characters.')
+            .max(200, 'Can be no longer than 200 characters')
+            .required('Required.'),
 }); 
 
 const Form: React.FC = () => {
@@ -24,22 +32,28 @@ const Form: React.FC = () => {
      console.log(data);
   };
   console.log(errors )
+  
     return (
         <form onSubmit={handleSubmit(submitForm)} noValidate className={classes.formContainer}>
             <TextField 
-               inputRef={register} 
-               label='Name' 
-               name='name ' 
-               fullWidth 
+               inputRef={register}
+               label="Name"
+               name="name"
+               fullWidth
+               variant="outlined"
+               error={!!errors.name}
+               helperText={errors.name ? errors.name.message : ''}
             />
             <TextField 
-               label='Content' 
-               name='content ' 
-               fullWidth 
-               multiline 
-               rows={3} 
-               variant='outlined' 
                inputRef={register}
+               multiline
+               rows={3}
+               label="Content"
+               name="content"
+               error={!!errors.content}
+               helperText={errors.content ? errors.content.message : ''}
+               fullWidth
+               variant="outlined"
             />
             <Box display='flex' justifyContent='center'>
                 <Button 
