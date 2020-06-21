e6236b078c45d7d37abd29681253c958657c9416
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 20:21:38 -0600

    Declare action in GuestBookModel

--- src/interfaces/GuestBookModel.ts
+++ src/interfaces/GuestBookModel.ts
@@ -1,6 +1,9 @@
 import GuestBookEntry from './GuestBookEntry'; 
+import { Action } from 'easy-peasy';
 
 // An array of Guest Book Entry
 export default interface GuestBookModel {
     entries: GuestBookEntry[];
+    addEntry:  Action<GuestBookModel, GuestBookEntry>; // 👈 declaring our action 
+                                                       // has two parameters, one the model and other the entry
 };
