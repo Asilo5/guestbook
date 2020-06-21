3aa6dbe0dbcc677783372cf08125addc627e1048
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-06-20 20:28:46 -0600

    Add action to GuestBook store

--- src/store/GuestBook.ts
+++ src/store/GuestBook.ts
@@ -1,12 +1,16 @@
 // Implement the guest book model
 import GuestBookModel from '../interfaces/GuestBookModel';
+import { action } from 'easy-peasy';
 
 const GuestBook: GuestBookModel = {
   entries: [{
       name: 'Consuelo',
       content: 'Hello World',
       submitted: new Date()
-  }] // set initial state of entries --> there are no guest input
+  }], // set initial state of entries --> there are no guest input
+  addEntry: action((state, entry ) => {
+    state.entries.push(entry);
+  })
 };
 
 export default GuestBook;
